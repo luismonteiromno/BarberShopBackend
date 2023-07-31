2431d42b6dd228e0d5709bfc8389c2e1a8b127ca
Luis Monteiro <felipemoreno9065@gmail.com>
2023-07-31 13:29:03 -0300

    Fix length

--- about_us/models.py
+++ about_us/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class AbousUs(models.Model):
     name = models.CharField('Nome', max_length=100, default='')
-    developed_by = models.CharField('Desenvolvido por:')
+    developed_by = models.CharField('Desenvolvido por:', max_length=150)
     description = models.TextField('Descrição', max_length=255)
     email = models.EmailField('Email para contato:', unique=True)
-    phone = models.CharField('Telefone para contato:')
+    phone = models.CharField('Telefone para contato:', max_length=50)
 
     def __str__(self):
         return f"{self.name} - {self.developed_by}"
