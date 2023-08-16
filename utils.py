a20cd3f78a7be96ec130db783c7f17aea65269e8
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-15 22:29:04 -0300

    Utils updated

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from barbershop import settings
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -37,3 +39,28 @@ def send_email(email, subject, body):
     except Exception as error:
         sentry_sdk.capture_exception(error)
         return 500
+
+
+def get_available_times_for_day(day, date):
+    working_start = datetime.combine(date, day.start)
+    working_end = datetime.combine(date, day.end_time)
+    pause_start = datetime.combine(date, day.pause_time) if day.pause_time else None
+    pause_end = datetime.combine(date, day.end_pause_time) if day.end_pause_time else None
+
+    available_times_list = []
+
+    current_time = working_start
+    time_interval = timedelta(hours=1)
+
+    while current_time.time() < working_end.time():
+        is_within_pause = (
+                pause_start and pause_end and
+                pause_start.time() <= current_time.time() < pause_end.time()
+        )
+
+        if not is_within_pause:
+            available_times_list.append(current_time.strftime('%H:%M'))
+
+        current_time += time_interval
+
+    return available_times_list
