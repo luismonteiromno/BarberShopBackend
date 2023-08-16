0f2704f3e26201b381e36e4f21dea3b95bd25ba5
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-15 22:28:06 -0300

    Models updated

--- barber_shop/models.py
+++ barber_shop/models.py
@@ -29,7 +29,7 @@ class Company(models.Model):
     street = models.CharField("Rua e número", max_length=150)
     instagram_link = models.URLField("Link do instagram", max_length=250, blank=True, null=True)
     facebook_link = models.URLField("Link do facebook", max_length=250, blank=True, null=True)
-    whatsapp_link = models.URLField('Link do Whatsapp', max_length=250, blank=True, null=True)
+    whatsapp_link = models.URLField("Link do whatsapp", max_length=250, blank=True, null=True)
     business_hours = JSONField(blank=True, null=True, verbose_name='Horário de Funcionamento')
     opening_hours = ArrayField(models.CharField(max_length=255), null=True, verbose_name='Horário de Funcionamento')
 
@@ -41,11 +41,30 @@ class Company(models.Model):
         verbose_name_plural = "Barbearias"
 
 
+class Days(models.Model):
+    day = models.CharField('Dia', max_length=50)
+    start = models.TimeField('Horário de inicio', null=True, auto_created=True)
+    end = models.TimeField('Horário de encerramento', null=True, auto_created=True)
+    pause_time = models.TimeField('Horário de pausa', blank=True, null=True, help_text='(opcional)')
+    end_pause_time = models.TimeField('Fim da pausa', blank=True, null=True, help_text='(opcional)')
+    company = models.ForeignKey(Company, verbose_name='Barbearia', null=True, blank=True, related_name='company_dat', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.day)
+
+    class Meta:
+        verbose_name = 'Dia'
+        verbose_name_plural = 'Dias de Funcionamento'
+
+
 class Schedules(models.Model):
+    barbershop = models.ForeignKey(Company, verbose_name='Barbearia', on_delete=models.PROTECT)
+    day = models.ForeignKey(Days, verbose_name='Dia', related_name='schedules_days', on_delete=models.PROTECT)
     client = models.ForeignKey('users.UserProfile', verbose_name='Cliente', related_name='client_schedules', on_delete=models.CASCADE)
     date = models.DateTimeField('Horário agendado')
     chosen_barber = models.ForeignKey('users.UserProfile', verbose_name='Barbeiro escolhido pelo cliente', related_name='client_chosen_barber', on_delete=models.CASCADE, null=True)
     confirmed_by_barber = models.BooleanField('Agendamento confirmado pelo barbeiro?', blank=True, null=True, default=False, help_text='Aguarde até o barbeiro confirmar o agendamento')
+    user_canceled = models.BooleanField('Cancelado pelo usuário?', default=False)
 
     def __str__(self):
         return str(self.client)
@@ -55,13 +74,14 @@ class Schedules(models.Model):
         verbose_name_plural = 'Agendamentos'
 
 
-class Days(models.Model):
-    day = models.CharField('Dia', max_length=50)
-    start = models.TimeField('Horário de inicio', null=True, auto_created=True)
-    end = models.TimeField('Horário de encerramento', null=True, auto_created=True)
-    pause_time = models.TimeField('Horário de pausa', blank=True, null=True, help_text='opcional')
-    company = models.ForeignKey(Company, verbose_name='Barbearia', related_name='company_dat', on_delete=models.CASCADE)
+class SchedulesDays(models.Model):
+    day = models.ForeignKey(Days, verbose_name='Dia', related_name='schedule_day', on_delete=models.PROTECT)
+    schedule = models.ForeignKey(Schedules, verbose_name='Agendado por', related_name='schedule', on_delete=models.PROTECT)
+    data = models.DateTimeField('Data')
+
+    def __str__(self):
+        return f"{self.day} - {self.schedule}"
 
     class Meta:
-        verbose_name = 'Dia'
-        verbose_name_plural = 'Dias de Funcionamento'
+        verbose_name = 'Dia agendado'
+        verbose_name_plural = 'Dias agendados'
