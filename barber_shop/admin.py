b69af383f83dbd0515b41f665bb9c173e53ac4e0
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-15 22:28:23 -0300

    Admin updated

--- barber_shop/admin.py
+++ barber_shop/admin.py
@@ -1,27 +1,23 @@
 from django.contrib import admin
-from .models import Company, Schedules, Days
+from .models import Company, Schedules, Days, SchedulesDays
 from django import forms
-from django.core.exceptions import ValidationError
-from django.http import HttpResponse
-from django.template import loader
-from xhtml2pdf import pisa
 
 
 class DaysInline(admin.StackedInline):
     model = Days
-    fields = ['day', 'start', 'end', 'pause_time']
+    fields = ['day', 'start', 'end', 'pause_time', 'end_pause_time']
 
 
 class CompanyAdmin(admin.ModelAdmin):
     fieldsets = (
-         ('Informações de Contato', {'fields': ('owner', 'employees', 'name', 'phone', 'instagram_link', 'facebook_link'
-                                                , 'whatsapp_link')}),
+         ('Informações de Contato', {'fields': ('owner', 'employees', 'name', 'phone', 'instagram_link', 'facebook_link')}),
          ('Informações de Endereço', {'fields': ('cep', 'state', 'city', 'neighborhood', 'street')}),
                  )
     inlines = [
         DaysInline
     ]
     filter_horizontal = ['owner', 'employees']
+    list_display = ['id', 'name']
 
 
 class FormSchedules(forms.ModelForm):
@@ -32,37 +28,31 @@ class FormSchedules(forms.ModelForm):
         user = request.user
         if user.type == 'barbeiro' or user.type == 'desenvolvedor_dono':
             self.fields['confirmed_by_barber'].disabled = False
-            self.fields['date'].disabled = False
-        else:
+            self.fields['user_canceled'].disabled = False
+        elif user.type == 'cliente':
             self.fields['confirmed_by_barber'].disabled = True
-            self.fields['date'].disabled = True
+            self.fields['user_canceled'].disabled = False
 
 
 class SchedulesAdmin(admin.ModelAdmin):
-    list_display = ['id', 'client', 'chosen_barber', 'date', 'confirmed_by_barber']
+    list_display = ['id', 'client', 'chosen_barber', 'date', 'day', 'confirmed_by_barber', 'user_canceled']
     list_filter = ['confirmed_by_barber']
     form = FormSchedules
-    actions = ['export_as_pdf']
 
-    @admin.action(description='Exportar Selecionados para PDF')
-    def export_as_pdf(self, obj, queryset):
-        context = {
-            "schedules": queryset
-        }
 
-        response = HttpResponse(content_type='schedules/pdf')
-        response['Content-Disposition'] = 'attachment; filename="schedules.pdf"'
+class SchedulesDaysAdmin(admin.ModelAdmin):
+    list_display = ['id', 'day', 'schedule', 'data']
 
-        template = loader.get_template('schedules.html')
-        html = template.render(context)
 
-        create_pdf = pisa.CreatePDF(html, dest=response)
-
-        if create_pdf.err:
-            raise ValidationError("Problema ao gerar PDF")
-
-        return response
+class DaysAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Modificar dia', {'fields': ('day', 'start', 'end', 'pause_time', 'end_pause_time', 'company')}),
+    )
+    list_display = ['day', 'start', 'end', 'company']
+    list_filter = ['company']
 
 
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Schedules, SchedulesAdmin)
+admin.site.register(SchedulesDays, SchedulesDaysAdmin)
+admin.site.register(Days, DaysAdmin)
